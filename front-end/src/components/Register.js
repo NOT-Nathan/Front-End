6cfdd030ca4d38b8621083306aa1d1e55724f86a
Greyson R Hamilton <greyson.r.hamilton@gmail.com>
2021-03-29 21:54:49 -0400

    Update Register.js

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,15 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-
+// check before you change your code. I may be wrong.
 const Register = ({user, setUser}) => {
     const {push} = useHistory();
     
     const register = e => {
+        // this doesn't need to be an event(e). pass setUser as parameter then run post request with setUser, not user. 
         e.preventDefault();
         axios.post('https://tt130bwplants.herokuapp.com/api/auth/register', user)
             .then(res => {
                 console.log(res);
+            // should localStorage be referenced here? localStorage.setItem("token", res.data.token);
+            // setUser(res.data.setUser);
                 setUser(res.data.new_user);
                 // localStorage.setItem('authToken', res.data.payload);
             })
